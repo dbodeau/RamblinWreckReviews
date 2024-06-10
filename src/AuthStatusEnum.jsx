a9bdc65e9a5dac3e2009138667d579e6a9d8d7b2
Wesley Woo <wwoo@mines.edu>
2024-06-10 16:06:25 -0600

    Admin Create Survey Questions

--- src/AuthStatusEnum.jsx
+++ src/AuthStatusEnum.jsx
@@ -1,7 +1,7 @@
+/* This Enum file is used to get user groups for the purpose of what the user sees in the navigation bar/page access.
+This File is mainly used in Wrapper. */
 const AuthStatusEnum = {
     ADMIN: "Admin",
     SUPERUSER: "Super-users",
     STUDENT: "Students"
 };
-  
-export default AuthStatusEnum;
