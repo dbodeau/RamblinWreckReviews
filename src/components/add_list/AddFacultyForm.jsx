b576c66bc09c720024ad4a68f1aa831a11eb720b
Andrew Nicola <acnicola444@gmail.com>
2024-09-26 23:05:59 -0600

    Faculty form now updated

--- src/components/add_list/AddFacultyForm.jsx
+++ src/components/add_list/AddFacultyForm.jsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { Button, CheckboxField, TextField } from '@aws-amplify/ui-react';
-import { IoMdClose } from "react-icons/io";
 
-export default function AddFaculty({formData, onSuccess}) {
+export default function AddFaculty({formData, onChange}) {
     /*  formData format:
         first_name: string
         last_name: string
@@ -10,33 +9,54 @@ export default function AddFaculty({formData, onSuccess}) {
         admin: bool
         professor: bool
     */
+    React.useEffect(() => {
+        // setup initial fields
+        onChange({
+            first_name: "",
+            last_name: "",
+            email: "",
+            admin: false,
+            professor: true
+        });
+    }, [onChange]);
 
-    const onSubmit = (event) => {
-        event.preventDefault();
+    const changeHandler = (e, field) => {
+        let value = e.currentTarget.value; // pull the value here cuz race conditions!!
+        // love amplify making events different
+        if (typeof(formData[field]) === "boolean") {
+            // breaks...
+            value = e.target.checked;
+        }
 
-    }
+        onChange((pfd) => {
+            const newPfd = {...pfd};
+            newPfd[field] = value;
+            return newPfd;
+        })
+    };
     
     return (
         <React.Fragment>
-            <Button><IoMdClose /></Button>
-            <form onSubmit={onSubmit}>
-                <TextField
-                    label="First Name"
-                />
-                <TextField
-                    label="Last Name"
-                />
-                <TextField
-                    label="Email"
-                />
-                <CheckboxField
-                    label="Admin"
-                />
-                <CheckboxField
-                    label="Professor"
-                />
-                <Button type="submit">Submit</Button>
-            </form>
+            <TextField
+                label="First Name"
+                onChange={(e) => changeHandler(e, 'first_name')}
+            />
+            <TextField
+                label="Last Name"
+                onChange={(e) => changeHandler(e, 'last_name')}
+            />
+            <TextField
+                label="Email"
+                onChange={(e) => changeHandler(e, 'email')}
+            />
+            <CheckboxField
+                label="Admin"
+                onChange={(e) => changeHandler(e, 'admin')}
+            />
+            <CheckboxField
+                label="Professor"
+                onChange={(e) => changeHandler(e, 'professor')}
+            />
         </React.Fragment>
     )
 }
