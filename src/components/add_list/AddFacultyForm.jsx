8956109e9a5c0541084003a3feaa3e31a1bf09d7
Andrew Nicola <acnicola444@gmail.com>
2024-10-08 20:12:37 -0600

    Add CWID field to faculty form

--- src/components/add_list/AddFacultyForm.jsx
+++ src/components/add_list/AddFacultyForm.jsx
@@ -6,6 +6,7 @@ const facultyFormData = {
     first_name: "",
     last_name: "",
     email: "",
+    cwid: "",
     role: "professor" // default to professor
 };
 
@@ -24,6 +25,10 @@ export default function AddFaculty({formData, onChange, onSubmit, onClose}) {
             errorMsg: ""
         },
         role: {
+            hasError: false,
+            errorMsg: ""
+        },
+        cwid: {
             hasError: false,
             errorMsg: ""
         }
@@ -63,6 +68,19 @@ export default function AddFaculty({formData, onChange, onSubmit, onClose}) {
                 hasError = true;
             } 
         }
+        if (!formData.hasOwnProperty("cwid") || formData.cwid === "") {
+            err.cwid.hasError = true;
+            err.cwid.errorMsg = "Required field";
+            hasError = true;
+        } else { // do additional validation
+            const pattern = new RegExp('^[0-9]{8}$'); // 8 numeric chars
+
+            if (!pattern.test(formData.cwid)) {
+                err.cwid.hasError = true;
+                err.cwid.errorMsg = "Invalid CWID";
+                hasError = true;
+            } 
+        }
 
         // update error state
         setErrors(err);
@@ -89,6 +107,13 @@ export default function AddFaculty({formData, onChange, onSubmit, onClose}) {
                     onChange={(e) => onChange(e, 'last_name')}
                     hasError={errors.last_name.hasError}
                     errorMessage={errors.last_name.errorMsg}
+                />
+                <TextField
+                    label="CWID"
+                    value={formData.cwid ?? ''}
+                    onChange={(e) => onChange(e, 'cwid')}
+                    hasError={errors.cwid.hasError}
+                    errorMessage={errors.cwid.errorMsg}
                 />
                 <TextField
                     label="Email"
