5612d14b7cff58b60fec3f8f73e74757b70c4cac
Andrew Nicola <acnicola444@gmail.com>
2024-10-30 12:20:55 -0600

    Form fields working

--- src/components/pages/admin/Admin_CreateDepartment.jsx
+++ src/components/pages/admin/Admin_CreateDepartment.jsx
@@ -5,22 +5,22 @@ import { SelectField, TextField, View } from "@aws-amplify/ui-react";
 import ContrastButton from '../../ContrastButton';
 
 export default function AdminCreateDepartment() {
+  const errors = {department: {hasError: false, errorMsg: ""}, email: {hasError: false, errorMsg: ""}, first: {hasError: false, errorMsg: ""}, last: {hasError: false, errorMsg: ""}};
   
-  const [formData, setFormData] = useState({department: "", email: ""});
-  const [formErrors, setFormErrors] = useState({department: {hasError: false, errorMsg: ""}, email: {hasError: false, errorMsg: ""}});
+  const [formData, setFormData] = useState({department: "", email: "", first: "", last: ""});
+  const [formErrors, setFormErrors] = useState(errors);
   const [departments, setDepartments] = useState([]);
 
-  const validate = () => {
-    console.log(formData);
+  const validate = (formData, field="all") => {
     let hasError = false;
-    const err = formErrors;
+    const err = errors;
     // do our data validation
-    if (!formData.hasOwnProperty("department") || formData.department === "") {
+    if ((field === "all" || field === "department") && formData.department === "") {
         err.department.hasError = true;
         err.department.errorMsg = "Required field";
         hasError = true;
-    } 
-    if (!formData.hasOwnProperty("email") || formData.email === "") {
+    }
+    if ((field === "all" || field === "email") && formData.email === "") {
       err.email.hasError = true;
       err.email.errorMsg = "Required field";
       hasError = true;
@@ -31,19 +31,29 @@ export default function AdminCreateDepartment() {
             err.email.hasError = true;
             err.email.errorMsg = "Invalid email";
             hasError = true;
-        } 
+        }
+    }
+    if ((field === "all" || field === "first") && formData.first === "") {
+      err.first.hasError = true;
+      err.first.errorMsg = "Required field";
+      hasError = true;
+    } 
+    if ((field === "all" || field === "last") && formData.last === "") {
+      err.last.hasError = true;
+      err.last.errorMsg = "Required field";
+      hasError = true;
     }
 
     // update error state
     setFormErrors(err);
-
     return hasError;
   }
 
   const onSubmit = () => {
     // createDepartment()
-    if (!validate()) {
+    if (!validate(formData)) {
       // createDepartment();
+      setFormData({department: "", email: "", first: "", last: ""});
     }
   }
 
@@ -53,9 +63,9 @@ export default function AdminCreateDepartment() {
     setFormData((pfd) => {
       const newFormData = {...pfd};
       newFormData[field] = value;
+      validate(newFormData, field);
       return newFormData;
     })
-    validate();
   }
 
   // onMount, grab departments
@@ -89,6 +99,22 @@ export default function AdminCreateDepartment() {
             onChange={(e) => onChange(e, 'email')}
             hasError={formErrors.email.hasError}
             errorMessage={formErrors.email.errorMsg}
+          />
+          <div style={{margin: "20px"}}/>
+          <TextField
+            label="First Name"
+            value={formData.first ?? ''}
+            onChange={(e) => onChange(e, 'first')}
+            hasError={formErrors.first.hasError}
+            errorMessage={formErrors.first.errorMsg}
+          />
+          <div style={{margin: "20px"}}/>
+          <TextField
+            label="Last Name"
+            value={formData.last ?? ''}
+            onChange={(e) => onChange(e, 'last')}
+            hasError={formErrors.last.hasError}
+            errorMessage={formErrors.last.errorMsg}
           />
           <div style={{display: "flex", flexDirection: "row-reverse", marginTop: "15px", }}> 
             <ContrastButton onClick={onSubmit}>Create</ContrastButton>
